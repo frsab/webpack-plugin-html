aab59448276fc49b4f64e0781ef8241e146ebf27
Saber FRAJ <frejsaber@gmail.com>
2022-08-03 16:51:36 +0200

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,42 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'), 
     filename: 'my-first-webpack.bundle.js',   
   },
-  watch:true
-      
+  watch:true,
+  // module:{
+  //   rules: [
+  //     {
+  //       test: /\.m?js$/,
+  //       exclude: /node_modules/,
+  //       use: ['babel-loader']
+  //       // use: {
+  //       //   loader: 'babel-loader',
+  //       //   options: {
+  //       //     presets:['env']
+  //       //     // presets: [
+  //       //     //   ['@babel/preset-env', { targets: "defaults" }]
+  //       //     // ],
+  //       //     // plugins: ['@babel/plugin-proposal-class-properties']
+  //       //   }
+  //       // }
+  //     }
+  //   ]
+  // }     
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env'
+            //  , { targets: "defaults" }
+            ]
+            ]
+          }
+        }
+      }
+    ]
+  } 
 };
