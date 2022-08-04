7be25761de5e780dc81bdf43885bc01e395564d1
Saber FRAJ <frejsaber@gmail.com>
2022-08-04 11:57:11 +0200

    add jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,13 @@
 // import log from ';/log.js';
+import $ from 'jquery';
 
 var log = require('./log.js')
 log("hello 1",new Date())
 log("hello2 ",new Date())
 log("hello III",new Date())
+
+$(document).ready(function(){
+    $("body").css("background-color","blue");
+  });
+
+
